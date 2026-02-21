e0d94af559b3c70f58fcf7efde49d268dc65ed22
mac <mac@MacBook.local>
2026-02-21 15:12:09 +0530

    Update logo path in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { UNITS, STORAGE_KEY } from './constants';
 import { submitToGoogleSheet } from './utils/googleSheets';
 import './App.css';
-// import logo from "./assets/sio-mc-logo.png";
+import logo from "../public/sio-mc-logo.png";
 
 const GOOGLE_SHEET_URL =
   'https://docs.google.com/spreadsheets/d/1LAnsqvavI0WA8dOOqaCi3iNxinnr4ZcndRmDF-x6XGs/edit?usp=sharing';
@@ -110,7 +110,8 @@ function App() {
     <div className="app">
       <header className="header">
         <div>
-          <img src="../public/sio-mc-logo.png" alt="Logo" className="logo-img" />
+          <img src={logo} alt="Logo" className="logo-img" />
+          
         </div>
         <p className="tagline">Collection Tracker</p>
       </header>
